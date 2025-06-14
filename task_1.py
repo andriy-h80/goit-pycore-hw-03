c3f5b459cbbf8bfcc3f2a6b2cc388bc1b5de7dd6
andriy-h80 <andrey.hanzel@gmail.com>
2025-06-14 20:04:25 +0300

    task 1 now done

--- task_1.py
+++ task_1.py
@@ -3,14 +3,16 @@
 Створіть функцію get_days_from_today(date), 
 яка розраховує кількість днів між заданою датою і поточною датою.
 '''
-# Спосіб 1 - timedelta
 from datetime import datetime, timedelta
 
 def get_days_from_today(date: str) -> int:
-    date_object = datetime.strptime(date, "%Y-%m-%d")
-    today = datetime.today()
-    difference = today - date_object
-    return difference.days
+    try:    
+        date_object = datetime.strptime(date, "%Y-%m-%d")
+        today = datetime.today()
+        difference = today - date_object
+        return difference.days
+    except ValueError:
+        return f"Цей формат {date} є неправильний. Будь ласка, введіть правильний формат"
 
 # Приклад. Дата в минулому 2020-10-09 - різниця +1709 днів
 day_in_past = get_days_from_today("2020-10-09")
@@ -18,7 +20,10 @@ day_in_past = get_days_from_today("2020-10-09")
 day_in_future = get_days_from_today("2025-10-09")
 # Приклад. Сьогоднішня дата - різниця 0 днів
 today_day = get_days_from_today("2025-06-14")
+# Приклад. Неправильний формат дати
+wrong_format = get_days_from_today("2025/06/14")
 
 print(f"Кількість днів складає: {day_in_past}")
 print(f"Кількість днів складає: {day_in_future}")
 print(f"Кількість днів складає: {today_day}")
+print(wrong_format)
